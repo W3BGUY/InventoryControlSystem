8f80c3f3c4c236859c1e0956fa915955da8a0f49
Charles Bastian <cbastian313@gmail.com>
2012-04-11 13:32:46 -0500

    adding initial project source files

---
+++ src/inventorycontrolsystem/InventoryControlSystem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventorycontrolsystem;
+
+/**
+ *
+ * @author cbastian
+ */
+public class InventoryControlSystem {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+}
